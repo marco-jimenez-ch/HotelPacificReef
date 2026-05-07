14d1d82613f2452bee10d5e500549634b3827992
Marco Jiménez <marco.jimenez@uc.cl>
2026-05-07 19:59:16 -0400

    fix: agregar CSRF_TRUSTED_ORIGINS para Azure

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-cambia-esta-clave-en-produccion')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://hotelpacificreef-app.azurewebsites.net', 'https://*.azurewebsites.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
